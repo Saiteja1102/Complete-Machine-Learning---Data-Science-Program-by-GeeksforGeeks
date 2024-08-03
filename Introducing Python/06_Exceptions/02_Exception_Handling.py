ce9147ffc92d54fa018758073cf6ffb9df42cfa6
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-08-03 16:40:19 +0530

    3

---
+++ Introducing Python/06_Exceptions/02_Exception_Handling.py
@@ -0,0 +1,35 @@
+# Exception Handling
+
+try:
+    x = 10
+    print(1)
+    print(x/0)
+    print(2)
+except:
+    print("There is a error!!")
+print("-"*50)
+
+# Exception with specific errors
+try:
+    x = 10
+    print(x/10)
+except ZeroDivisionError:
+    print("Zero in the division")
+except:
+    print("Unknown Error!!")
+print("-"*50)
+
+# Final Exception with specific error
+try:
+    lst = [1,2,3,4]
+    # print(lst[2])
+    print(lst[6])
+except IndexError as ie:
+    print(ie)
+except Exception as e:
+    print(e)
+else:
+    print("There is no error")
+finally:
+    print("Program End")
+    print("-"*50)
