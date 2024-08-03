91f16a4fcccf23afcaf801104f4a274d4fe97097
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-08-03 16:11:29 +0530

    3

---
+++ Introducing Python/06_Exceptions/01_Errors.py
@@ -0,0 +1,24 @@
+# Syntax Error
+# print("hello)
+
+# Runtime Error
+# print(20/0)
+
+# Logical error
+def sum(a,b):
+    return a-b
+print(sum(1,3))
+
+# name Error
+# print(Hello)
+
+# Value Error
+# print(int("3.14"))
+
+# Index Error
+lst = [1,2,3,4,5]
+# print(lst[10])
+
+# Key Error
+dct = {1:"a",2:"b",3:"c"}
+# print(dct[6])
