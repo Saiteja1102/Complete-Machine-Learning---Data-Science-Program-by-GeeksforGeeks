0ef919b2ca0760af4338ec567cee854df3b4d43d
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-08-03 13:54:04 +0530

    3

---
+++ Introducing Python/05_Special Functions/02_ASCII_Codes.py
@@ -0,0 +1,19 @@
+# ASCII Codes
+# ord function
+print(ord("A"))
+print(ord(" "))
+print(ord("7"))
+print(ord("{"))
+print("-"*20)
+
+# chr function
+print(chr(45))
+print(chr(77))
+print(chr(99))
+print(chr(121))
+print("-"*20)
+
+# All Codes
+for i in range(33,127):
+    print(i,"-",chr(i))
+print("-"*20)
